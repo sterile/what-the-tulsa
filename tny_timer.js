3a468be4fc5285c14139080b82ffc4f7707003f0
Brandon Berney <brandonberney@protonmail.com>
2019-04-10 16:27:59 -0400

    Mom I did a thing

--- tny_timer.js
+++ tny_timer.js
@@ -1,19 +1,40 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Review Assignment
 
    Event Timer
-   Author: 
-   Date:   
+   Author: Brandon Berney
+   Date:   April 10 2019
 
 */
+showClock();
 
+function showClock() {
+    var thisDay = new Date("May 19, 2018 9:31:27 a.m.");
+    var localDate = thisDay.toDateString();
+    var localTime = thisDay.toTimeString();
+    document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>";
+
+    var j4Date = nextJuly4(thisDay);
+    j4Date.setHours(21);
+
+    var days = (j4Date - thisDay) / (1000 * 60 * 60 * 24);
+    var hrs = (days - Math.floor(days)) * 24;
+    var mins = (hrs - Math.floor(hrs)) * 60;
+    var secs = (mins - Math.floor(mins)) * 60;
+
+    document.getElementById("dLeft").textContent = Math.floor(days);
+    document.getElementById("hLeft").textContent = Math.floor(hrs);
+    document.getElementById("mLeft").textContent = Math.floor(mins);
+    document.getElementById("sLeft").textContent = Math.floor(secs);
+}
 
 function nextJuly4(currentDate) {
-   var cYear = currentDate.getFullYear();
-   var jDate = new Date("July 4, 2018");
-   jDate.setFullYear(cYear);
-   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
-   return jDate;
+    var cYear = currentDate.getFullYear();
+    var jDate = new Date("July 4, 2018");
+    jDate.setFullYear(cYear);
+    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+    return jDate;
 }
